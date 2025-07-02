f4b4f40392b0e35a1a9e4b650bd15d423e8118bf
borhanauto <autosparehouse01@gmail.com>
2025-07-03 01:19:04 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Website Loaded: Borhan Auto Parts Centre");
-});
-document.querySelectorAll('.buy-button').forEach(button => {
-  button.addEventListener('click', function(event) {
-    event.preventDefault();
-    const productName = this.closest('.product-card').querySelector('h3').innerText;
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.push(productName);
-    localStorage.setItem('cart', JSON.stringify(cart));
-    alert(`${productName} added to cart!`);
-  });
-});
+
 function toggleDescription(card) {
   document.querySelectorAll('.product-card').forEach(c => {
     if (c !== card) c.classList.remove('active');
