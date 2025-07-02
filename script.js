a78e273ddbf70737c15c224254d310dfc62ea1c5
borhanauto <autosparehouse01@gmail.com>
2025-07-03 01:32:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function toggleDescription(card) {
   });
   card.classList.toggle('active');
 }
+function filterProducts() {
+  const search = document.getElementById("searchInput").value.toLowerCase();
+  const category = document.getElementById("categoryFilter").value;
+
+  document.querySelectorAll(".product-card").forEach(card => {
+    const title = card.querySelector("h3").textContent.toLowerCase();
+    const productCategory = card.getAttribute("data-category");
+
+    const matchCategory = (category === "all" || productCategory === category);
+    const matchSearch = title.includes(search);
+
+    if (matchCategory && matchSearch) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
