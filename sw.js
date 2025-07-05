8bf95f05d98b037d35d44ee2d871f39be754e9ac
borhanauto <autosparehouse01@gmail.com>
2025-07-06 00:50:37 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("borhan-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
