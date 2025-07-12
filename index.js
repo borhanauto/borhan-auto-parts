ea8ab437c24bae8a0dabb29ce545e940c4ce9712
borhanauto <autosparehouse01@gmail.com>
2025-07-12 13:41:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const path = require('path');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
